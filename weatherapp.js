e66dc59683639093e6a34337438d5175d7b480bd
ThomasLam2142 <thomaslam2142@gmail.com>
2023-08-22 13:46:45 -0400

    api is working. currently displaying toronto weather

--- weatherapp.js
+++ weatherapp.js
@@ -1,11 +1,32 @@
 const apiKey = 'fdc60c470455a2ced54c787c389f2a43';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={fdc60c470455a2ced54c787c389f2a43}';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=fdc60c470455a2ced54c787c389f2a43';
+
+const tempValueElement = document.querySelector('.tempValue');
+const humidValueElement = document.querySelector('.humidValue');
+const descriptionValueElement = document.querySelector('.descriptionValue');
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    // Process the data here
-    console.log(data);
+    let temperature = data.main.temp;
+    let humidity = data.main.humidity;
+    let description = data.weather[0].description;
+    
+    // Display the weather information
+    console.log(`Temperature: ${temperature}°C`);
+    console.log(`Humidity: ${humidity}%`);
+    console.log(`Description: ${description}`);
+    temperature = temperature - 273.15;
+    temperature = Math.round(temperature);
+
+    //tempValueElement = temperature - 273.15;
+    tempValueElement.textContent = `${temperature}°C`;
+
+    humidValueElement.textContent = `${humidity}%`
+
+    descriptionValueElement.textContent = `${description}`
+
+
   })
   .catch(error => {
     console.error('Error:', error);
